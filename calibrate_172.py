2bd0bf0ab11c13d25800d542f23fabc533e85f4e
nwright-mcc <32554608+nwright-mcc@users.noreply.github.com>
2023-06-07 09:27:31 -0400

    Update raw_input for python 3

--- calibrate_172.py
+++ calibrate_172.py
@@ -7,16 +7,21 @@ import time
 import json
 import io
 
+# raw_input isn't defined in Python3.x, whereas input wasn't behaving like raw_input in Python 2.x
+# this should make both input and raw_input work in Python 2.x/3.x like the raw_input from Python 2.x 
+try: input = raw_input
+except NameError: raw_input = input
+    
 # get serial number
-serial =  raw_input("Enter serial number: ")
+serial =  input("Enter serial number: ")
 
 slopes = [0.0, 0.0]
 offsets = [0.0, 0.0]
 
-slopes[0] =  float(raw_input("Enter ch 0 slope:  "))
-offsets[0] = float(raw_input("Enter ch 0 offset: "))
-slopes[1] =  float(raw_input("Enter ch 1 slope:  "))
-offsets[1] = float(raw_input("Enter ch 1 offset: "))
+slopes[0] =  float(input("Enter ch 0 slope:  "))
+offsets[0] = float(input("Enter ch 0 offset: "))
+slopes[1] =  float(input("Enter ch 1 slope:  "))
+offsets[1] = float(input("Enter ch 1 offset: "))
 
 # Create calibration file
 data = {}
